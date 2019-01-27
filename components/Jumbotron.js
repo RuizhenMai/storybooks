c3ba22feeadac11d6441793be6f55692779ad7c5
RuizhenMai <mairuizhen1998@gmail.com>
2019-01-26 20:15:38 -0500

    bug fixed

---
+++ components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Alice Wonderland</h1>
+      <small> You can see this because you have signed in </small>
+
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore nisi
+        sed unde magni vitae ea, laborum nobis. Nam natus, ipsum sequi delectus
+        placeat incidunt ipsam iusto iure, numquam id aperiam!
+      </p>
+    </div>
+  );
+}
